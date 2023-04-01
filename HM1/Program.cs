fa876913e6c98841f96b9474ee3e2390848fd707
grib <nikita-gribkov@inbox.ru>
2023-04-02 01:55:53 +0300

    рабочая

--- HM1/Program.cs
+++ HM1/Program.cs
@@ -1,15 +1,37 @@
-﻿Console.Write("Введите количество строк, которые хотите ввести: ");
-            int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число.
-            string[] strs = new string[n]; //Объявляем массив строк длиной n (которую ввёл пользователь)
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("Введите строку №{0}:\r\n    ", i + 1);
-                strs[i] = Console.ReadLine(); //Заполняем его
-            }
-            Console.WriteLine("Вы ввели следующие строки:");
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine(strs[i]);
-            }
-            Console.Write("Нажмите [Enter] для выхода...");
-            Console.ReadLine();
+﻿
+Console.Write("Введите количество строк, которые хотите ввести: ");
+int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число.
+string[] strs = new string[n]; //Объявляем массив строк длиной n (которую ввёл пользователь)
+int k = 0;
+int j = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+    strs[i] = Console.ReadLine(); //Заполняем его
+}
+Console.WriteLine("Искомые элементы массива :");
+for (int i = 0; i < n;)
+{
+    if (strs[i].Length > 3){
+        i++;
+    } else {
+        k++;
+        i++;
+    }
+}
+string[] result = new string[k];
+for (int i = 0; i < n;)
+{
+if (strs[i].Length > 3){
+        i++;
+    } else {
+        result[j]=strs[i];
+        i++;
+        j++;
+    }
+}
+for (int i = 0; i < k;i++)
+{
+    Console.Write(result[i]+ " ");
+}
+Console.ReadLine();
