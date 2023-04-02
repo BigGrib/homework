65cd5e97bdefd7b03edb59d5f7d1239df44a2cc6
grib <nikita-gribkov@inbox.ru>
2023-04-02 23:04:45 +0300

    7 ready

--- 7/Program.cs
+++ 7/Program.cs
@@ -1,16 +1,17 @@
-﻿int Summ(int i){
-    if (i==0) return 0;
-    else if (i==1) return 1;
-    else return Summ(i-1)+Summ(i-2);
+﻿int Summ(int i, int j, int k){
+    if (i==0) return j;
+    else if (i==1) return k;
+    else return Summ(i-1,j,k)+Summ(i-2,j,k);
 
 }
 
-
-
-
 Console.Write("N = ");
 int N = int.Parse(Console.ReadLine());
+Console.Write("N1 = ");
+int N1 = int.Parse(Console.ReadLine());
+Console.Write("N2 = ");
+int N2 = int.Parse(Console.ReadLine());
 for (int i = 0;i<=N;i++){
-int result = Summ(i);
+int result = Summ(i,N1,N2);
 Console.WriteLine(result);
 }
