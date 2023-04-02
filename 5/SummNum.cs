15af4262222fd9f4aeae41b126617d747ee7dd55
grib <nikita-gribkov@inbox.ru>
2023-04-03 00:36:41 +0300

    Сделана задача на сумму цифр

---
+++ 5/SummNum.cs
@@ -0,0 +1,12 @@
+﻿int Sum(int num)
+    {
+        if (num < 10) return num;
+        else return num % 10 + Sum(num / 10);
+    }
+
+
+Console.WriteLine("Введите число для вычисления суммы его чисел");
+Console.Write("число = ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"сумма чисел числа {num} = {Sum(num)}");
