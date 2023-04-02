a423b065e5eb5b6c43ecf3b9cde212924e1f24d6
grib <nikita-gribkov@inbox.ru>
2023-04-02 22:54:13 +0300

    7

---
+++ 7/Program.cs
@@ -0,0 +1,16 @@
+﻿int Summ(int i){
+    if (i==0) return 0;
+    else if (i==1) return 1;
+    else return Summ(i-1)+Summ(i-2);
+
+}
+
+
+
+
+Console.Write("N = ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 0;i<=N;i++){
+int result = Summ(i);
+Console.WriteLine(result);
+}
